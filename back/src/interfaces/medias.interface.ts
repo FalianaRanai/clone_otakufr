befc8dc003c49e78005c02fdfdf0d7c30be97f7c
Faliana <faliana23@gmail.com>
2025-03-23 19:29:07 +0300

    init

---
+++ back/src/interfaces/medias.interface.ts
@@ -0,0 +1,14 @@
+export interface Media {
+  id_media?: number;
+  titre: string;
+  sygnopsis: string;
+  autre_nom: string;
+  auteur: string;
+  realisateur: string;
+  studio: string;
+  date_sortie: string;
+  duree: string;
+  id_type: number;
+  id_statut: number;
+  affiche: string;
+}
